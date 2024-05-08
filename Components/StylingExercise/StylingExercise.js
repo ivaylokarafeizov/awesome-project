2fcc57b677bf76af08b901658d99744ab035cea8
ivayloK <qwerty36758@gmail.com>
2024-05-08 17:39:08 +0300

    Moved the first exercise in a separate component folder

---
+++ Components/StylingExercise/StylingExercise.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import ColorBox from './ColorBox';
+
+const StylingExercise = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>
+        Here are some boxes of different colors
+      </Text>
+      <ColorBox colorName="Cyan" colorHex="#2aa198" />
+      <ColorBox colorName="Blue" colorHex="#268bd2" />
+      <ColorBox colorName="Magenta" colorHex="#d33682" />
+      <ColorBox colorName="Orange" colorHex="#cb4b16" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+  heading: {
+    color: 'black',
+    fontSize: 19,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
+
+export default StylingExercise;
