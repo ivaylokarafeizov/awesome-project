3ebbe64ba22d69d675e90cc2a100a163d30eae7d
ivayloK <qwerty36758@gmail.com>
2024-05-10 18:17:40 +0300

    Home page - style update

--- Components/Home.js
+++ Components/Home.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const Home = ({ navigation }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <TouchableOpacity onPress={() => navigation.navigate('StylingExercise')}>
-        <Text>Styling Exercise</Text>
+        <Text style={styles.title}>1. Styling Exercise</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate('ListExercise')}>
-        <Text>List Exercise</Text>
+        <Text style={styles.title}>2. List Exercise</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => navigation.navigate('NavigationExercise')}
       >
-        <Text>Navigation Exercise</Text>
+        <Text style={styles.title}>3. Navigation Exercise</Text>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 10,
+    paddingHorizontal: 10,
+  },
+  title: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
 
 export default Home;
